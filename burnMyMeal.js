d8a8e7a6f100f747cb8bde72fe31b1285013e8fa
nbmc1991 <nbmc1991@gmail.com>
2020-10-27 21:45:26 -0400

    updates to javascript

--- burnMyMeal.js
+++ burnMyMeal.js
@@ -1,23 +1,61 @@
-// $(document).ready(function(){
+$(document).ready(function(){
+
+  var minCal = 100-300;
+  
+  function getFood (calories){
+    console.log(calories)
+    var settings = {
+      "async": true,
+      "crossDomain": true,
+      "url": "https://api.edamam.com/search?app_id=868b7c06&app_key=952e33eb5cdae0482626e48f05b6ba1f&q=sugar&calories=100-" + calories ,
+      "method": "GET",
+     
+    }
+ $.ajax(
+  settings
+
+ ).then(function(response){
+console.log(response)
+
+ })
+};
+
+$(".fire").on("click",function(){
+getFood($(this).attr("data-calories"));
+
+
+
+
+})
+
+
+  
+
+
+
     // var instance = M.Carousel.init({
     //     fullWidth: true,
     //     indicators: true
     //   });
+    
     //   // Or with jQuery
+    
     //   $('.carousel.carousel-slider').carousel({
     //     fullWidth: true,
     //     indicators: true
     //   });
-    function display(){
-      // var calorie300 = 300;
-      // var APIKey = "AIzaSyCg1nUbYturyaMWVeypxr6rMBs6i9HVN5I&q"   
-      var queryURL = "https://www.googleapis.com/youtube/v3/search?key=AIzaSyCg1nUbYturyaMWVeypxr6rMBs6i9HVN5I&q=300calorieworkout"
-              $.ajax({
-                url: queryURL,
-                method: "GET"
-              }).then(function(response) {
-                console.log(response);
-              })
-      }
-      display();  
-// });
+          
+      // function display(){
+
+      //   // var calorie300 = 300;
+      //   // var APIKey = "AIzaSyCg1nUbYturyaMWVeypxr6rMBs6i9HVN5I&q"   
+      //   var queryURL = "https://www.googleapis.com/youtube/v3/search?key=AIzaSyCg1nUbYturyaMWVeypxr6rMBs6i9HVN5I&q=300calorieworkout"
+      //           $.ajax({
+      //             url: queryURL,
+      //             method: "GET"
+      //           }).then(function(response) {
+      //             console.log(response);
+      //           })
+      //   }
+      //   display();  
+});

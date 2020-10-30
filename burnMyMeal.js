95009607e6f8b070f82ebdfcafe0b57dd8047eaa
Denny da Vjncj <daVjncjCode@gmail.com>
2020-10-30 14:43:16 -0400

    version control

--- burnMyMeal.js
+++ burnMyMeal.js
@@ -33,98 +33,45 @@ $(document).ready(function () {
       };
     });
   };
+//  $.ajax({
+//   settings
+//  })
+//  .then(function(response){
+//   $(response).each(function(i, recepie){
+//     // $("#recepie-drop").append($("<tr>").append($("<td>").append(recepie.)
+//   })
+//    $("#recepie-drop").recepie.text();
+// displayFood()
+//  })
+$(".fire").on("click",function(){
+getFood($(this).attr("data-calories"));
+//three functions to fire
 
-  $("#fire300").on("click", function () {
-    fire300($(this).attr("data-calories"));
-  });
-
-  $("#fire500").on("click", function () {
-    fire500($(this).attr("data-calories"));
-  });
-
-  $("#fire1000").on("click", function () {
-    fire1000($(this).attr("data-calories"));
-  });
-
-  function fire500(five) {
-    var settings5 = {
-      "async": true,
-      "crossDomain": true,
-      // "url": "https://api.edamam.com/search?app_id=868b7c06&app_key=952e33eb5cdae0482626e48f05b6ba1f&q=desserts&calories=100-300",
-      "url": "https://api.edamam.com/search?q=cake&app_id=bc8179cb&app_key=022c930d7b4fd86ded9d2a31ca7f7d9e&from=0&to=3&calories=301-500",
-      "method": "GET",
-    }
-    $.ajax(
-      settings5
-    ).then(function (response) {
-      // console.log(response)
-      var calories = response.hits[0].recipe.calories;
-      console.log(calories)
-      var recipes = response.hits;
-      // console.log(recipes)
-      for (var i = 0; i < 2; i++) {
-        var recipeObj = recipes[i]
-        // console.log(recipeObj)
-        $("<div>").addClass("card")
-        var rowDiv = $("<div>").addClass("row");
-        var colDiv = $("<div>").addClass("col s12 m6");
-        var cardDiv = $("<div>").addClass("card");
-        var cardImg = $("<img>").addClass("card-image").attr("src", recipeObj.recipe.image)
-        var card = rowDiv.append(colDiv, cardDiv, cardImg)
-        $("#recipe-drop").append(card)
-
-        console.log(settings5);
-
-        }
-      });
-    }
-
-      function fire1000(ten) {
-        var settings10 = {
-          "async": true,
-          "crossDomain": true,
-          // "url": "https://api.edamam.com/search?app_id=868b7c06&app_key=952e33eb5cdae0482626e48f05b6ba1f&q=desserts&calories=100-300",
-          "url": "https://api.edamam.com/search?q=brownies&app_id=bc8179cb&app_key=022c930d7b4fd86ded9d2a31ca7f7d9e&from=0&to=3&calories=501-1000",
-          "method": "GET",
-        }
-        $.ajax(
-          settings10
-        ).then(function (response) {
-          // console.log(response)
-          var calories = response.hits[0].recipe.calories;
-          console.log(calories)
-          var recipes = response.hits;
-          // console.log(recipes)
-          for (var i = 0; i < 2; i++) {
-            var recipeObj = recipes[i]
-            // console.log(recipeObj)
-            $("<div>").addClass("card")
-            var rowDiv = $("<div>").addClass("row");
-            var colDiv = $("<div>").addClass("col s12 m6");
-            var cardDiv = $("<div>").addClass("card");
-            var cardImg = $("<img>").addClass("card-image").attr("src", recipeObj.recipe.image)
-            var card = rowDiv.append(colDiv, cardDiv, cardImg)
-            $("#recipe-drop").append(card)
-
-            console.log(settings10);
-
-          }
-        });
-      }
-
+});
+});
 
 //programmatic iFrame player
+//empty variable for taking in userInput/capturing click event-> let userRequest; can be input or clickEvent
+//iFrame shouldn't appear
+
+//pay attention to scope
+  //nest click event within function, maybe not
+
 let tag=document.createElement('script');
-tag.src="https://www.youtube.com/iframe_api";
+tag.src="https://www.youtube.com/iframe_api/search?"+userPref+"q=";//query url to be built out 
 let firstScriptTag=document.getElementsByTagName('script')[0];
 firstScriptTag.parentNode.insertBefore(tag,firstScriptTag);
 
+  //target needed for specific parameters[properties]
+  //for loop to parse through object response
   let player;
-  function onYouTubeIframeAPIReady(){
+  let userPref=$(this).attr("data-calories");//give buttons dataAttr
+  function onYouTubeIframeAPIReady(response){
+    console.log(response)
   player=new YT.Player('player',{
     height:'100%',
     width:'100%',
-    videoId:'M7lc1UVf-VE',
+    videoId:'',//store user query, click or otherwise:val()
     events:{
       'onReady':onPlayerReady,
       // 'onStateChange':onPlayerStateChange
@@ -133,17 +80,4 @@ firstScriptTag.parentNode.insertBefore(tag,firstScriptTag);
 }
 function onPlayerReady(e){
   e.target.playVideo();
-
-develop GET request ASAP!!!
-$.ajax({
-  url:queryURL,//HYPOTHETICAL
-  method:"GET"
-}).then(function(response){
-  console.log(response);
-  what do I want to happen after I fetch user request
-})
-$(".fire").on("click",function searchByKeyword{
-  let results 
- }
-})
-    })
+}

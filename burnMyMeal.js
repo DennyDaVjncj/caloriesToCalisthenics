6a3274d9b194774511198ef80341928a46004170
Denny da Vjncj <daVjncjCode@gmail.com>
2020-11-06 12:02:39 -0500

    updated ajax call, not complete

--- burnMyMeal.js
+++ burnMyMeal.js
@@ -10,8 +10,6 @@ $(document).ready(function () {
     $.ajax(
       settings
     ).then(function (response) {
-      // console.log(response)
-
 
       var name = response.hits[0].recipe.label;
       console.log(name)
@@ -41,17 +39,7 @@ $(document).ready(function () {
         var cardName = $("<span>").addClass("card-title").attr("data-name", name);
         var card = rowDiv.append(colDiv, cardDiv, cardDiv2, cardImg, name);
         var card2 = colDiv.append(calories);
-        $("#recipe-drop").append(card, card2);
-
-        $("#apiFulFill").on("click", function () {
-          let qURL = "https://www.youtube.com/watch?v=" + "M7lc1UVf-VE";
-
-
-        })
-
-
-        // $(".card").append(url);
-
+        $("#recipe-drop").append(card, card2);       
       };
     });
   };
@@ -59,14 +47,13 @@ $(document).ready(function () {
   function fire500(five) {
     var settings5 = {
       "async": true,
-      "crossDomain": true,
-      // "url": "https://api.edamam.com/search?app_id=868b7c06&app_key=952e33eb5cdae0482626e48f05b6ba1f&q=desserts&calories=100-300",
+      "crossDomain": true,      
       "url": "https://api.edamam.com/search?q=cake&app_id=bc8179cb&app_key=022c930d7b4fd86ded9d2a31ca7f7d9e&from=0&to=3&calories=301-500",
       "method": "GET",
     }
     $.ajax(
       settings5
-    ).then(function (response) {
+    ).then(function(response) {
 
       var name = response.hits[0].recipe.label;
       console.log(name)
@@ -105,10 +92,7 @@ $(document).ready(function () {
       }
     });
   }
-
   function fire1000(ten) {
-
-
 
     var settings10 = {
       "async": true,
@@ -153,7 +137,6 @@ $(document).ready(function () {
         $("#recipe-drop").append(card, card2);
 
         console.log(settings10);
-
       }
     });
   }
@@ -161,69 +144,69 @@ $(document).ready(function () {
   $("#fire300").on("click", function () {
     insertIFrame(300);
     $("#recipe-drop").empty();
-    fire300($(this).attr("data-calories"));
+    // fire300($(this).attr("data-calories"));
   });
 
   $("#fire500").on("click", function () {
     insertIFrame(500);
     $("#recipe-drop").empty();
-    fire500($(this).attr("data-calories"));
+    // fire500($(this).attr("data-calories"));
   });
 
   $("#fire1000").on("click", function () {
     insertIFrame(1000);
     $("#recipe-drop").empty();
-    fire1000($(this).attr("data-calories"));
+    // fire1000($(this).attr("data-calories"));    
   });
-
-  // function vidCall(){
-  //   ;
-  //   qUrl+specific
-  // }
-
 })
-
 // modal trigger
 $('.modal').modal();
 
-let videoId = "";
-// this happens on click and takes in either calories === 300 or 500 or 1000, based on which button you clicked
-function insertIFrame(calories) {
-  console.log(calories);
-  // this is where you change the videoId based on the calories number
-  // videoId is a global variable so when onYouTubeIframeAPIReady() runs, videoId will be different depending on the button you clicked on 
-  if (calories === 300) {
-    videoId = "1fDHrKRqy34";
-  } else if (calories === 500) {
-    videoId ="LeOHID-EDA0";
-  } else if (calories === 1000) {
-    videoId = "VUpGLzYJJNM";
-  }
-  //ajax call needs configuring in order to provide the most dynamic experience to repeat/unique users
+// let videoId = "";//perhaps this will take from our returned object
 
+function insertIFrame(calories){
+  let Vquery="&q="+calories+" calories!";//.syntax to find vid id property within object response
   
+  let apiKey="&key=AIzaSyCg1nUbYturyaMWVeypxr6rMBs6i9HVN5I"//get the KEY! google Dev Console
+  let qURL="https://cors-anywhere.herokuapp.com/https://youtube.googleapis.com/youtube/v3/search?part=snippet"+Vquery+apiKey;
+  $.ajax({
+    url:qURL,
+    method:"GET"
+  }).then(function(Data){
+    console.log(Data);
+    let videoId=Data.items[0].id.videoId//[i], for looping through objectValues
+    onYouTubeIframeAPIReady(videoId);
+  })
+
+    //hardcoded version in providing "dynamic" responses
+  // console.log(calories);   
+  // if (calories === 300) {
+  //   videoId = "1fDHrKRqy34";
+  // } else if (calories === 500) {
+  //   videoId ="LeOHID-EDA0";
+  // } else if (calories === 1000) {
+  //   videoId = "VUpGLzYJJNM";
+  // }
+  //ajax call needs configuring in order to provide the most dynamic experience to repeat/unique users 
 
   //programmatic iFrame player
   var tag = document.createElement('script');
-
   tag.src = "https://www.youtube.com/iframe_api";
   var firstScriptTag = document.getElementsByTagName('script')[0];
   firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);
 }
 
-var player;
-function onYouTubeIframeAPIReady() {
+let player;
+function onYouTubeIframeAPIReady(videoId) {
   player = new YT.Player('player', {
     height: '390',
     width: '640',
-    videoId,//shortcut for videoId: videoId,
+    videoId,//shortcut for videoId: videoId
     events: {
       'onReady': onPlayerReady,
-      //'onStateChange': onPlayerStateChange
     }
   });
 }
-
-function onPlayerReady(event) {
+function onPlayerReady(event){
   event.target.playVideo();
 }
